e7b2bf3a0abbcaff5ad361eedab4000f64e0929d
Heidi4 <erickkaloki28@gmail.com>
2024-07-07 18:04:11 +0300

    Refactoring code

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
 import 'package:ticket_app/base/widgets/app_double_text.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
-import 'package:ticket_app/screens/widgets/hotel.dart';
+import 'package:ticket_app/screens/home/widgets/hotel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -107,7 +107,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     AppDoubleText(
                       bigText: 'Hotels',
                       smallText: 'View all',
-                      func: () => Navigator.pushNamed(context, "/all_tickets"),
+                      func: () {},
                     ),
                     const SizedBox(
                       height: 20,
